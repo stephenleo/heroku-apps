1fe0f33c3c43f05a493f674b34ebd3ce31777f2a
stephenleo <stephen.leo87@gmail.com>
2021-08-01 16:10:59 +0800

    updating requirements and procfile

--- src/backend/main.py
+++ src/backend/main.py
@@ -114,7 +114,7 @@ def predict(instances: List[str]):
 
 @app.get('/')
 def health_check():
-    return {'success': True, 'status': 200}
+    return {'message': 'This is the homepage of the API', 'success': True, 'status': 200}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port='80')
