8152d9736dec2316562f403c634a521da11c8be6
stephenleo <stephen.leo87@gmail.com>
2021-08-09 20:26:21 +0800

    adding rest, faq

---
+++ src/boyorgirl/faq.md
@@ -0,0 +1,8 @@
+1. **How does it work?**
+    - "Boy or Girl?" uses an Artificial Intelligence algorithm to take in a name and output whether the name is a boy's name or a girl's name.
+    - The algorithm has learnt how to make these output predictions by learning from over 35,000 English first names.
+2. **Does it work for non-English names?**
+    - The algorithm was developed by feeding it English names and hence has the highest accuracy on English names. However, I did notice decent accuracy on Indian names since they share the same root language in the past. The accuracy seems to be bad on Chinese or non-English sounding names.
+3. **Do you store my information?**
+    - NO! The algorithm runs on the names your provide and plots out the ouput predictions. The names are lost after that and the app does not store any information
+
--- src/boyorgirl/main.py
+++ src/boyorgirl/main.py
@@ -45,19 +45,36 @@ app.index_string = """
 # Server
 server = app.server
 
+# FAQ section
+with open('faq.md', 'r') as file:
+    faq = file.read()
+
 # App Layout
 app.layout = html.Table([
     html.Tr([
         html.H1(html.Center(html.B('Boy or Girl?'))),
+        html.Div(
+            html.Center("Check if your names are Boy's names or Girl's names"),
+            style={'fontSize': 20}),
         html.Br(),
         html.Div(
-            dbc.Input(id='names', value='Joe Biden, Kamala Harris',
+            dbc.Input(id='names',
+                      value='Joe Biden, Kamala Harris',
                       placeholder='Enter names separated by space or comma',
                       style={'width': '700px'})),
         html.Br(),
-        html.Center(
-            dbc.Button(
-                'Submit', id='submit-button', n_clicks=0, color='primary')),
+        html.Center(children=[
+            dbc.Button('Submit',
+                       id='submit-button',
+                       n_clicks=0,
+                       color='primary',
+                       type='submit'),
+            dbc.Button('Reset',
+                       id='reset-button',
+                       color='secondary',
+                       type='submit',
+                       style={"margin-left": "50px"})
+        ]),
         html.Br(),
         dcc.Loading(id='table-loading',
                     type='default',
@@ -68,7 +85,12 @@ app.layout = html.Table([
         html.Br(),
         dcc.Loading(id='chart-loading',
                     type='default',
-                    children=html.Div(id='bar-plot', children=[]))
+                    children=html.Div(id='bar-plot', children=[])),
+        html.Br(),
+        html.Div(
+            html.Center(html.B('About "Boy or Girl?"')),
+            style={'fontSize': 20}),
+        dcc.Markdown(faq, style={'width': '700px'})
     ])
 ],
                         style={
@@ -77,85 +99,102 @@ app.layout = html.Table([
                         })
 
 
+
 # Callbacks
+@app.callback([Output('submit-button', 'n_clicks'),
+               Output('names', 'value')], Input('reset-button', 'n_clicks'),
+              State('names', 'value'))
+def update(n_clicks, value):
+    if n_clicks is not None and n_clicks > 0:
+        return -1, ''
+    else:
+        return 0, value
+
+
 @app.callback(
     [Output('predictions', 'children'),
      Output('selected-names', 'data')], Input('submit-button', 'n_clicks'),
     State('names', 'value'))
 def predict(n_clicks, value):
-    # Split on all non-alphabet characters
-    names = re.findall(r"\w+", value)
+    if n_clicks >= 0:
+        # Split on all non-alphabet characters
+        names = re.findall(r"\w+", value)
 
-    # Restrict to first 10 names only
-    names = names[:10]
+        # Restrict to first 10 names only
+        names = names[:10]
 
-    # Predictions
-    pred_df = pd.DataFrame(pred_model.predict(names)).drop_duplicates()
+        # Predictions
+        pred_df = pd.DataFrame(pred_model.predict(names)).drop_duplicates()
 
-    return [
-        dash_table.DataTable(
-            id='pred-table',
-            columns=[{
-                'name': col,
-                'id': col,
-            } for col in pred_df.columns],
-            data=pred_df.to_dict('records'),
-            filter_action="native",
-            filter_options={"case": "insensitive"},
-            sort_action="native",  # give user capability to sort columns
-            sort_mode="single",  # sort across 'multi' or 'single' columns
-            page_current=0,  # page number that user is on
-            page_size=10,  # number of rows visible per page
-            style_cell={
-                'fontFamily': 'Open Sans',
-                'textAlign': 'center',
-                'padding': '10px',
-                'backgroundColor': 'rgb(255, 255, 204)',
-                'height': 'auto',
-                'font-size': '16px'
-            },
-            style_header={
-                'backgroundColor': 'rgb(0, 0, 255)',
-                'color': 'white',
-                'textAlign': 'center'
-            },
-            export_format='csv'
-        )
-    ], names
+        return [
+            dash_table.DataTable(
+                id='pred-table',
+                columns=[{
+                    'name': col,
+                    'id': col,
+                } for col in pred_df.columns],
+                data=pred_df.to_dict('records'),
+                filter_action="native",
+                filter_options={"case": "insensitive"},
+                sort_action="native",  # give user capability to sort columns
+                sort_mode="single",  # sort across 'multi' or 'single' columns
+                page_current=0,  # page number that user is on
+                page_size=10,  # number of rows visible per page
+                style_cell={
+                    'fontFamily': 'Open Sans',
+                    'textAlign': 'center',
+                    'padding': '10px',
+                    'backgroundColor': 'rgb(255, 255, 204)',
+                    'height': 'auto',
+                    'font-size': '16px'
+                },
+                style_header={
+                    'backgroundColor': 'rgb(128, 128, 128)',
+                    'color': 'white',
+                    'textAlign': 'center'
+                },
+                export_format='csv')
+        ], names
+    else:
+        return [], ''
 
 
-@app.callback(
-    Output('bar-plot', 'children'),
-    [Input('predictions', 'children'),
-     Input('selected-names', 'data')])
-def bar_plot(data, selected_names):
-    # Bar Chart
-    data = pd.DataFrame(data[0]['props']['data'])
-    fig = px.bar(data,
-                 x="Probability",
-                 y="Name",
-                 color='Boy or Girl?',
-                 orientation='h',
-                 color_discrete_map={
-                     'Boy': 'dodgerblue',
-                     'Girl': 'lightcoral'
-                 })
+@app.callback(Output('bar-plot', 'children'), [
+    Input('submit-button', 'n_clicks'),
+    Input('predictions', 'children'),
+    Input('selected-names', 'data')
+])
+def bar_plot(n_clicks, data, selected_names):
+    if n_clicks >= 0:
+        # Bar Chart
+        data = pd.DataFrame(data[0]['props']['data'])
+        fig = px.bar(data,
+                     x="Probability",
+                     y="Name",
+                     color='Boy or Girl?',
+                     orientation='h',
+                     color_discrete_map={
+                         'Boy': 'dodgerblue',
+                         'Girl': 'lightcoral'
+                     })
 
-    fig.update_layout(title={
-        'text': 'Confidence in Prediction',
-        'x': 0.5
-    },
-                      yaxis={
-                          'categoryorder': 'array',
-                          'categoryarray': selected_names,
-                          'autorange': 'reversed',
-                      },
-                      xaxis={'range': [0, 1]},
-                      font = {'size': 14},
-                      width=700)
+        fig.update_layout(title={
+            'text': 'Confidence in Prediction',
+            'x': 0.5
+        },
+                          yaxis={
+                              'categoryorder': 'array',
+                              'categoryarray': selected_names,
+                              'autorange': 'reversed',
+                          },
+                          xaxis={'range': [0, 1]},
+                          font={'size': 14},
+                          width=700)
 
-    return [dcc.Graph(figure=fig)]
+        return [dcc.Graph(figure=fig)]
+    else:
+        return []
 
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
